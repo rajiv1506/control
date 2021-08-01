1f50cfcefc035356a76e0aa1e0fcb2f3103fbb34
rajiv1506 <rajiv.pratap26@gmail.com>
2021-08-02 05:12:41 +0530

    Testing jenkins installation

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,10 @@
-node 172.31.34.166{
+node ip-172-31-47-12{
   include role::dbserver
   include role::appserver
   include role::webserver
   include role::exec
 }
 
-node ip-172-31-47-192{
-  include role::jenkins
-}
 
 node ip-172-31-39-75{
   include role::jenkins
